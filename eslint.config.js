08db3bb1c8e52691af4d80267bb8fa7028b01cad
Lawjay Lee <lawjaylee1@icloud.com>
2026-03-23 17:05:24 -0700

    batman

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/'],
+  }
+);
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div class="p-4">
+    <h1 class="text-2xl font-bold">imgcomp</h1>
+  </div>
+`;
