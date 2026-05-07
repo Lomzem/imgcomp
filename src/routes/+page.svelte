da6a57c0581f2118ce87f1097e09b5adf83c7d02
Lawjay Lee <lawjaylee1@icloud.com>
2026-05-07 04:53:31 -0700

    feat: file upload disappears when file selected

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-  import Input from "$lib/components/ui/input/input.svelte"
+  import FileUpload from "./components/FileUpload.svelte"
   import { fileUpload } from "./fileUpload.svelte"
-
-  $inspect(fileUpload.file)
 </script>
 
-<main class="w-sm place-self-center">
-  <form>
-    <Input type="file" accept="image/*" name="file" bind:files={fileUpload.files} required />
-  </form>
-</main>
+{#if !fileUpload.file}
+  <FileUpload />
+{/if}
---
+++ src/routes/components/FileUpload.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Input from "$lib/components/ui/input/input.svelte"
+  import { slide } from "svelte/transition"
+  import { fileUpload } from "../fileUpload.svelte.ts"
+</script>
+
+<main out:slide={{ duration: 300 }} class="w-sm place-self-center">
+  <form>
+    <Input type="file" accept="image/*" name="file" bind:files={fileUpload.files} required />
+  </form>
+</main>
