3d1b8e5d2ee881472d52e46d5f8210a2cdd8ab42
Lawjay Lee <lawjaylee1@icloud.com>
2026-05-07 09:36:20 -0700

    feat: GitHub link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
   import FileUpload from "./components/FileUpload.svelte"
   import Output from "./components/Output.svelte"
   import { imageUpload } from "./imageUpload.svelte.ts"
+  import { IconBrandGithubFilled } from "@tabler/icons-svelte"
 </script>
 
 <main class="w-svw place-self-center p-4">
@@ -14,3 +15,16 @@
     </div>
   {/if}
 </main>
+
+<footer class="absolute">
+  <a
+    href="https://github.com/Lomzem/imgcomp"
+    target="_blank"
+    rel="noreferrer"
+    aria-label="View on GitHub"
+    class="mt-4 ml-4 flex gap-1 text-muted-foreground transition hover:text-foreground"
+  >
+    <IconBrandGithubFilled />
+    <span class="self-baseline-last">view on github</span>
+  </a>
+</footer>
